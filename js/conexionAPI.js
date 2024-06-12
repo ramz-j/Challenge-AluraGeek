b095b4eabad16f19aae423048e6c769586711f0c
Jessica Ramirez <ramzgonz.j@gmail.com>
2024-06-11 23:51:16 -0400

    Sustitucion del server-json local al server-jason vercel

--- js/conexionAPI.js
+++ js/conexionAPI.js
@@ -1,12 +1,12 @@
 async function listarProductos() {
-    const conexion = await fetch("http://localhost:3001/productos");
+    const conexion = await fetch("https://alura-geek-api-ivory.vercel.app/productos");
     const conexionConvertida = conexion.json();
     
     return conexionConvertida;
 }
 
 async function enviarProductos(nombre,imagen,precio){
-    const conexion = await fetch("http://localhost:3001/productos",{
+    const conexion = await fetch("https://alura-geek-api-ivory.vercel.app/productos",{
         method: "POST",
         headers: {"Content-type":"application/json"},
         body:JSON.stringify({
@@ -20,7 +20,7 @@ async function enviarProductos(nombre,imagen,precio){
 }
 
 async function borrarProducto(id){
-    const conexion = await fetch(`http://localhost:3001/productos/${id}`,{
+    const conexion = await fetch(`https://alura-geek-api-ivory.vercel.app/productos/${id}`,{
         method:"DELETE"
     })
 }
